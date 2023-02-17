9914c5b2d03203a3db5a4cdc940a206c621ffcf6
Barışcan Sargın <bariscansargin4@gmail.com>
2023-02-17 14:54:36 +0300

    Added database with mongoDB

--- app.js
+++ app.js
@@ -1,9 +1,51 @@
+require('dotenv').config()
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose")
+var _ = require("lodash")
 const app = express();
-const date = require(`${__dirname}/date.js`) 
-let items = []
-let workItems = []
+
+
+
+
+
+try {
+    mongoose.connect(process.env.MONGO_URL) //!Connection with mongoDB
+        .then(input => console.log("Connected MongoDB"))
+} catch (error) {
+    console.log(error);
+
+}
+
+//* DB Model's 
+const itemSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: String
+    }
+})
+
+const Item = mongoose.model("Item", itemSchema)
+
+const item1 = new Item({
+    name: "Welcome to your todolist !"
+})
+const item2 = new Item({
+    name: "Hit the + button to add a new item."
+})
+const item3 = new Item({
+    name: "<-- Hit this to delete an item."
+})
+const defaultItems = [item1, item2, item3];
+
+const listSchema = mongoose.Schema({
+    name: String,
+    items: [itemSchema]
+})
+const List = mongoose.model("List", listSchema)
+
+
+
 
 
 
@@ -14,40 +56,107 @@ app.use(express.static("public"))
 
 /// Rendering root path.
 app.get("/", (req, res) => {
-    res.render("list", { listName: date.getDate() , newListItems: items });
+
+    Item.find((error, foundItems) => {
+
+        if (foundItems.length === 0) {
+            Item.insertMany(defaultItems, (error) => {
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log("Added")
+                }
+            })
+            res.redirect("/")
+        } else {
+            res.render("list", { listName: "Today", newListItems: foundItems })
+        }
+    })
+
+
+
 
 })
 //  Rendering work path.
 
-app.get("/work", (req, res) => {
-    res.render("list", { listName: "Works", newListItems: workItems })
+
+app.get("/:customListName", (req, res) => { //TODO : CREATE DYNAMİC ROUTE ! 
+    const customListName = _.capitalize(req.params.customListName)
+    List.findOne({ name: customListName }, (err, foundList) => {
+        if (!err) {
+            if (!foundList) {
+                console.log("Doesn't exist.")
+                const list = new List({
+                    name: customListName,
+                    items: defaultItems
+                })
+                list.save()
+                res.redirect(`/${customListName}`)
+
+            } else {
+                res.render("list", { listName: foundList.name, newListItems: foundList.items })
+
+            }
+
+        }
+    })
 
 })
+
+
 app.get("/about", (req, res) => {
     res.render("about")
 })
 // Routing 
 app.post("/", (req, res) => {
-    let item = req.body.newItem;
-    if (item !== "") {
-        if (req.body.list === "Works") {
-            workItems.push(item)
-            res.redirect("/work") //Routing work path
-        } else {
-            items.push(item)
-            res.redirect("/");
-        }
+    const itemName = req.body.newItem;
+    const listName = req.body.list
+
+
+    const item = new Item({
+        name: itemName
+    })
+    if (listName === "Today") {
+        item.save()
+        res.redirect("/")
     } else {
-        if (req.body.list === "Works") {
-            res.redirect("/work")
-        } else {
-            res.redirect("/")
-        }
+        List.findOne({ name: listName }, (err, foundList) => {
+            foundList.items.push(item)
+            foundList.save()
+            res.redirect(`/${listName}`)
+        })
     }
-    console.log(req.body)
-    
 
 
+
+})
+
+app.post("/delete", (req, res) => {
+    const checkedItemId = req.body.checkbox
+    const checkedListName = req.body.listName
+    console.log(req.body);
+    if (checkedListName === "Today") {
+        Item.deleteOne({ _id: checkedItemId }, error => {  //* req.body.checkbox === value of checkbox in list.ejs *//
+            if (error) {
+                console.log(error)
+            }
+        })
+        res.redirect("/")
+    } else {
+        List.findOne({ name: checkedListName }, (err, foundList) => {
+            if (!err) {
+
+                
+                List.findOneAndUpdate({name:checkedListName},{$pull:{items:{_id:checkedItemId}}}, (err, result) => {
+                    if(!err){
+                        res.redirect(`/${checkedListName}`)
+                    }
+                })
+
+
+            }
+        })
+    }
 })
 
 // Server listener
